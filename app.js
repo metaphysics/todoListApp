7d77070b53004f73e9505841163c147c2ad5276f
Lux <lux@mystic.io>
2019-04-12 08:18:59 -0400

    Implement the toLocalDateString() function in app.js, and clear the scriptlets and if statement from list.ejs

--- app.js
+++ app.js
@@ -11,38 +11,16 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res) {
 
   var today = new Date();
-  currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 1:
-      day = "Monday"
-      break;
-    case 2:
-      day = "Tuesday"
-      break;
-    case 3:
-      day = "Wednesday"
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    case 0:
-      day = "Sunday";
-      break;
-
-    default:
-      day = "Day Unknown";
-      console.log("Error: currentDay equals: " + currentDay);
-
+  var dateFormat = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   }
 
+  day = today.toLocaleString("en-US", dateFormat);
+
+  //EJS
   res.render("list", {
     kindOfDay: day
   });
