150d775a9f157af672b95ec1595d27615b5a5585
Lux <lux@mystic.io>
2019-04-13 18:18:39 -0400

    Just prior to creating our own module.

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 
-//let item = "";
 let items = [];
 let workItems = [];
 
@@ -37,11 +36,19 @@ app.get("/", function(req, res) {
 
 });
 
+app.get("/work", function(req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
 app.post("/", function(req, res) {
     let item = req.body.newItem;
 
     if (req.body.list === "Work") {
-      workItems.push(item);
+      workItems.push(item);m
       res.redirect("/work");
     } else {
       items.push(item);
@@ -49,20 +56,6 @@ app.post("/", function(req, res) {
     }
 
 });
-
-
-app.get("/work", function(req, res) {
-  res.render("list", { listTitle: "Work List", newListItems: workItems });
-});
-
-// app.post("/work", function(req, res) {
-//   let item = req.body.newItem;
-//
-//   workItems.push(item);
-//
-//   res.redirect("/work");
-//
-// });
 
 
 app.listen(3000, function() {
