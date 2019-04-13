24e453012e27d39c96d9ae09da5545d9f775eb5e
Lux <lux@mystic.io>
2019-04-13 17:56:45 -0400

    Just prior to adding header and footer templates

--- app.js
+++ app.js
@@ -40,10 +40,14 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
     let item = req.body.newItem;
 
-    items.push(item);
+    if (req.body.list === "Work") {
+      workItems.push(item);
+      res.redirect("/work");
+    } else {
+      items.push(item);
+      res.redirect("/")
+    }
 
-    // Redirect to app.get() for rendering
-    res.redirect("/");
 });
 
 
@@ -51,15 +55,14 @@ app.get("/work", function(req, res) {
   res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.post("/work", function(req, res) {
-  let item = req.body.newItem;
-
-  workItems.push(item);
-
-  res.redirect("/work");
-
-});
-
+// app.post("/work", function(req, res) {
+//   let item = req.body.newItem;
+//
+//   workItems.push(item);
+//
+//   res.redirect("/work");
+//
+// });
 
 
 app.listen(3000, function() {
